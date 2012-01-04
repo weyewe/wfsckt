0ebcae4532ed275a9fc22999752a4254e9d1bcf7
weyewe <w.yunnal@gmail.com>
2012-01-04 23:41:05 +0700

    innitial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,13 @@
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require libs/jquery-1.6.1.min
+//= require libs/jQuerytools
+//= require libs/jquery.backstretch
+//= require libs/jquery.scrolltotop
+//= require libs/noie6.js
+//= require libs/jquery.hoverIntent.min
+//= require fully
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wifisocket::Application.config.secret_token = 'becc33478821e5639638b0eb23306ccc258f25c9f207686776e3eb95086bd5ea89218ad9d48c3c03f99d562f91835a556450bd82eaed978775f498d9e0f2bcfd'
